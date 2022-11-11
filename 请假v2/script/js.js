9b9dfe9c1423314d592e633c624331dceb4e7130
twistedkfc <68820128+twistedkfc@users.noreply.github.com>
2022-11-11 11:05:07 +0800

    Update js.js

--- 请假v2/script/js.js
+++ 请假v2/script/js.js
@@ -2,19 +2,19 @@
 let date = new Date()   
 let td = new Date()
 //开始时间，当前时间前30min
-td.setMinutes(date.getMinutes() - 0)
+td.setMinutes(date.getMinutes() - 30)
 $id('kssj2').innerHTML = `${zero(td.getMonth())}-${zero(td.getDay())} ${zero(td.getHours())}:${zero(td.getMinutes())}`
 //结束时间，当前时间后三个小时
-td.setHours(date.getHours() + 0)
+td.setHours(date.getHours() + 3)
 $id('jssj2').innerHTML = `${zero(td.getMonth())}-${zero(td.getDay())} ${zero(td.getHours())}:${zero(td.getMinutes())}`
 
 //发起申请的时间,2小时38分钟之前
-td.setHours(date.getHours() - 0)
-td.setMinutes(td.getMinutes() - 0)
+td.setHours(date.getHours() - 2)
+td.setMinutes(td.getMinutes() - 38)
 $id('fqsj').innerHTML = `${zero(td.getMonth())}-${zero(td.getDay())} ${zero(td.getHours())}:${zero(td.getMinutes())}`
 //审批通过时间，1小时19分钟之前
-td.setHours(date.getHours() - 0)
-td.setMinutes(td.getMinutes() - 0)
+td.setHours(date.getHours() - 1)
+td.setMinutes(td.getMinutes() - 19)
 $id('spsj').innerHTML = `${zero(td.getMonth())}-${zero(td.getDay())} ${zero(td.getHours())}:${zero(td.getMinutes())}`
 
 //如何销假？
